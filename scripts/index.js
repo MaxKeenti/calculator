818ee999edeaa892deeaa0841b69dbc93f6dbd8c
maximilianogonzalezcalzada <maxgonzalezcalzada@gmail.com>
2025-10-17 16:08:25 -0600

    fix: index.js, changed variable names on code

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,23 @@
 console.log("Hola mundo de la calculadora");
 
-let numero1;
-let numero2;
+let a;
+let b;
 let operation;
 let state;
 
 // Operaciones
-const OPERACION_SUMA = 1;
-const OPERACION_RESTA = 2;
-const OPERACION_MULTIPLICACIONA = 3;
-const OPERACION_DIVISION = 4;
-const OPERACION_PORCENTAJE = 5;
+const OPERATION_ADD = 1;
+const OPERATION_SUBTRACT = 2;
+const OPERATION_TIMES = 3;
+const OPERATION_DIVIDE = 4;
+const OPERATION_PERCENTAGE = 5;
 
 // Estados
-const ESTADO_INICIAL = 1;
-const ESTADO_CAPTURA_A = 2;
-const ESTADO_CALCULO = 3;
-const ESTADO_CAPTURA_B = 4;
-const ESTADO_RESULTADO = 5;
+const STATE_ZERO = 1;
+const STATE_CAPTURE_A = 2;
+const STATE_CAPTURE_OPERATION = 3;
+const STATE_CAPTURE_B = 4;
+const STATE_EQUALS = 5;
 
 // Acciones
 const ACTION_NUMBER = 1;
@@ -40,29 +40,29 @@ function load() {
 
   let buttonSuma = document.getElementById("buttonAdd");
   buttonSuma.addEventListener("click", function () {
-    operate(OPERACION_SUMA);
+    operate(OPERATION_ADD);
   });
-  let buttonResta = document.getElementById("buttonResta");
+  let buttonResta = document.getElementById("buttonSubstract");
   buttonResta.addEventListener("click", function () {
-    operate(OPERACION_RESTA);
+    operate(OPERATION_SUBTRACT);
   });
-  let buttonMultiplica = document.getElementById("buttonMultiplica");
+  let buttonMultiplica = document.getElementById("buttonTimes");
   buttonMultiplica.addEventListener("click", function () {
-    operate(OPERACION_MULTIPLICACIONA);
+    operate(OPERATION_TIMES);
   });
-  let buttonDivide = document.getElementById("buttonDivision");
+  let buttonDivide = document.getElementById("buttonDivide");
   buttonDivide.addEventListener("click", function () {
-    operate(OPERACION_DIVISION);
+    operate(OPERATION_DIVIDE);
   });
-  let buttonPorcentaje = document.getElementById("buttonPorcentaje");
+  let buttonPorcentaje = document.getElementById("buttonPercentage");
   buttonPorcentaje.addEventListener("click", function () {
-    operate(OPERACION_PORCENTAJE);
+    operate(OPERATION_PERCENTAGE);
   });
-  let buttonCalcular = document.getElementById("buttonCalcular");
+  let buttonCalcular = document.getElementById("buttonEquals");
   buttonCalcular.addEventListener("click", function () {
     stateMachine(ACTION_RESULT);
   });
-  state = ESTADO_INICIAL;
+  state = STATE_ZERO;
 }
 
 ///////// Funciones de la calculadora
@@ -112,26 +112,26 @@ function operate(operator) {
 }
 
 function stateMachine(action, parameter) {
-  if (state == ESTADO_INICIAL && action == ACTION_NUMBER) {
-    state = ESTADO_CAPTURA_A;
-  } else if (state == ESTADO_CAPTURA_A && action == ACTION_NUMBER) {
-    state == ESTADO_CAPTURA_A;
-  } else if (state == ESTADO_CAPTURA_A && action == ACTION_OPERATION) {
-    state = ESTADO_CALCULO;
+  if (state == STATE_ZERO && action == ACTION_NUMBER) {
+    state = STATE_CAPTURE_A;
+  } else if (state == STATE_CAPTURE_A && action == ACTION_NUMBER) {
+    state == STATE_CAPTURE_A;
+  } else if (state == STATE_CAPTURE_A && action == ACTION_OPERATION) {
+    state = STATE_CAPTURE_OPERATION;
     operation = parameter;
-    numero1 = parseInt(document.getElementById("display").value);
+    a = parseInt(document.getElementById("display").value);
     cleanDisplay();
-  } else if (state == ESTADO_CALCULO && action == ACTION_OPERATION) {
-    state = ESTADO_CALCULO;
-  } else if (state == ESTADO_CALCULO && action == ACTION_NUMBER) {
+  } else if (state == STATE_CAPTURE_OPERATION && action == ACTION_OPERATION) {
+    state = STATE_CAPTURE_OPERATION;
+  } else if (state == STATE_CAPTURE_OPERATION && action == ACTION_NUMBER) {
     console.log("cambiando a estado de capturando segundo numero");
-    state = ESTADO_CAPTURA_B;
+    state = STATE_CAPTURE_B;
   } else if (
-    state == ESTADO_CAPTURA_B &&
+    state == STATE_CAPTURE_B &&
     (action == ACTION_OPERATION || action == ACTION_RESULT)
   ) {
-    numero2 = parseInt(document.getElementById("display").value);
-    state = ESTADO_RESULTADO;
+    b = parseInt(document.getElementById("display").value);
+    state = STATE_EQUALS;
     resultado = calcular();
   }
 }
@@ -139,16 +139,16 @@ function stateMachine(action, parameter) {
 function calcular() {
   console.log(
     "Calculando: " +
-      numero1 +
+      a +
       " --- " +
-      numero2 +
+      b +
       " --- " +
       state +
       " --- " +
       operation
   );
-  if (operation === OPERACION_SUMA) {
-    let resultado = suma(numero1, numero2);
+  if (operation === OPERATION_ADD) {
+    let resultado = suma(a, b);
     let display = document.getElementById("display");
     display.value = resultado;
   }
