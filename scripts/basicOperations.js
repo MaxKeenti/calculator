25700aa0ca037892b6529da6cd5c62b0a5218c28
maximilianogonzalezcalzada <maxgonzalezcalzada@gmail.com>
2025-10-17 19:59:52 -0600

    feat: basic operations module exported

--- scripts/basicOperations.js
+++ scripts/basicOperations.js
@@ -1,27 +1,27 @@
-console.log(add(3, 6));
-console.log(substract(3, 6));
-console.log(times(3, 6));
-console.log(divide(3, 6));
-console.log(percentage(10, 150));
-
+// Basic operations
 function add(a, b) {
   return a + b;
 }
-
-function substract(a, b) {
+function subtract(a, b) {
   return a - b;
 }
-
 function times(a, b) {
   return a * b;
 }
-
 function divide(a, b) {
   return a / b;
 }
-
 function percentage(a, b) {
   return (a * b) / 100;
 }
 
-//TO DO: máquina de estadoss
+// Map operation constants to their functions
+const operationHandlers = {
+  [OPERATION_ADD]: add,
+  [OPERATION_SUBTRACT]: subtract,
+  [OPERATION_TIMES]: times,
+  [OPERATION_DIVIDE]: divide,
+  [OPERATION_PERCENTAGE]: percentage,
+};
+
+export { add, subtract, times, divide, percentage, operationHandlers };
